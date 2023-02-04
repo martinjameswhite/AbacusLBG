fb204e8eac1a3df72f51aa9e22612972c431a6c7
Martin White <martinjameswhite@users.noreply.github.com>
2023-02-03 18:37:05 -0800

    Override want_rsd.

--- fake_lbg.py
+++ fake_lbg.py
@@ -26,8 +26,9 @@ class MockLBG:
         self.meta = get_meta(self.sim_params['sim_name'],\
                              redshift=self.sim_params['z_mock'])
         #
-        # additional parameter choices
-        self.want_rsd      = self.HOD_params['want_rsd']
+        # additional parameter choices -- we will apply RSD
+        # ourselves so override the setting in HOD_params.
+        self.want_rsd      = False
         self.write_to_disk = self.HOD_params['write_to_disk']
         #
         # Load the mask.
