9eec5e17a50c8d0da44dc1ebdc5c755d013c8daa
Martin White <martinjameswhite@users.noreply.github.com>
2023-02-27 10:59:14 -0800

    Include field rotation and data mask.

--- mc_lbg_wx.py
+++ mc_lbg_wx.py
@@ -4,9 +4,14 @@
 # computing their clustering.
 #
 import numpy as np
-from   fake_lbg import MockLBG
-from   calc_wx  import calc_wx
 
+from   fake_lbg  import MockLBG
+from   calc_wx   import calc_wx
+from   in_mask   import SurveyMask
+from   rotate_to import rotate_to
+
+from astropy.table     import Table
+from scipy.interpolate import InterpolatedUnivariateSpline as Spline
 
 
 
@@ -15,6 +20,20 @@ if __name__=="__main__":
     # Set up a random number generator with a
     # fixed seed for reproducability.
     rng    = np.random.default_rng(1)
+    # Set the name of the field we'll work with and load
+    # the mask, random catalog and radial selection fn.
+    fname= "xmmlss"
+    sfn  = np.loadtxt("selection_fn.txt")
+    mask = SurveyMask('clauds-{:s}-hpmask.fits'.format(fname))
+    tt   = Table.read('clauds-{:s}-rands-dens10000.fits'.format(fname))
+    # Set the center of the field.
+    cra = np.median(tt['RA'])
+    cdc = np.median(tt['DEC'])
+    print("Setting field center to ({:f},{:f})".format(cra,cdc),flush=True)
+    # Mask the randoms.
+    ww,ran     = mask(tt['RA'],tt['DEC']),{}
+    ran['RA' ] = tt['RA' ][ww]
+    ran['DEC'] = tt['DEC'][ww]
     # Define the mock catalog, shell and HOD.
     # 0  12.15  13.55   0.30   1.00   0.75   0.03   6.79e-04
     lbgs   = MockLBG('hod_big.yaml',None,4383.)
@@ -24,58 +43,41 @@ if __name__=="__main__":
     lbgs.generate()
     lbgs.assign_lum(0.25)
     # Select a field so we have access to the Lside etc.
-    diam   = 3.2 * np.pi/180.
+    diam   = 4.5 * np.pi/180.
     lbgs.select(diam,[0.,0.,0.])
     chi0   = lbgs.d['chi0']
     Lside  = lbgs.d['Lside']
     Lx     = Lside/chi0 * 180./np.pi
     Ly     = Lside/chi0 * 180./np.pi
     ichi   = 1.0/  chi0 * 180./np.pi
-    # Now we want to determine the sampling fraction to
-    # get the right angular number density.
-    ntarget,nbar = 1000.0,[]
-    for i in range(25):
-        # Generate the galaxies.
-        offset = rng.uniform(low=-0.5,high=0.5,size=3)
-        lbgs.select(diam,offset)
-        nobj   = lbgs.d['nkeep']
-        nbar.append(nobj / (Lx*Ly))
-    fsamp = ntarget / np.median(nbar)
     # Match the min/max values of chi.
     chimin,chimax = np.min(lbgs.zpos+chi0),np.max(lbgs.zpos+chi0)
-    # Generate a uniform random catalog.
-    # We offset the RA to eliminate negative RAs
-    # just to avoid a warning.
-    nran = 8000000
-    ran  = {}
-    ran['RA' ] = rng.uniform(low=-Lx/2.,high=Lx/2. ,size=nran) + Lx
-    ran['DEC'] = rng.uniform(low=-Ly/2.,high=Ly/2. ,size=nran)
-    # apply mask.
-    rad2 = ( (ran['RA']-Lx)**2 + (ran['DEC'])**2 )*(np.pi/180.)**2
-    ran['RA' ] = ran['RA' ][rad2<diam**2/4]
-    ran['DEC'] = ran['DEC'][rad2<diam**2/4]
+    print("Raw 3D number density ",lbgs.d['nbar'])
+    sampfn  = Spline(sfn[:,1],1e-3/lbgs.d['nbar']*sfn[:,2],k=1,ext='zeros')
     # Now do the MC loop.
     rval,wxs,ngals = None,[],[]
-    for i in range(64):
+    for i in range(256):
         # Generate the galaxies.
         offset = rng.uniform(low=-0.5,high=0.5,size=3)
         lbgs.select(diam,offset)
         dat        = {}
-        dat['RA' ] = lbgs.xpos*ichi + Lx
+        dat['RA' ] = lbgs.xpos*ichi
         dat['DEC'] = lbgs.ypos*ichi
-        dat['CHI'] = lbgs.zpos + chi0
-        # downsample
+        dat['CHI'] = lbgs.zpos+chi0
+        # Apply radial selection function.
         rand = rng.uniform(low=0,high=1,size=dat['RA'].size)
-        ww   = np.nonzero( rand<fsamp )[0]
+        ww   = np.nonzero( rand<sampfn(dat['CHI']) )[0]
         dat['RA' ] = dat['RA' ][ww]
         dat['DEC'] = dat['DEC'][ww]
         dat['CHI'] = dat['CHI'][ww]
-        # apply mask.
-        rad2 = ( (dat['RA']-Lx)**2 + (dat['DEC'])**2 )*(np.pi/180.)**2
-        dat['RA' ] = dat['RA' ][rad2<diam**2/4]
-        dat['DEC'] = dat['DEC'][rad2<diam**2/4]
-        dat['CHI'] = dat['CHI'][rad2<diam**2/4]
-        # Make a random selection of 10% of the objects.
+        # Rotate the objects to the field center and apply mask.
+        nra,ndc    = rotate_to(dat['RA'],dat['DEC'],cra,cdc)
+        ww         = mask(nra,ndc)
+        dat['RA' ] = nra[ww]
+        dat['DEC'] = ndc[ww]
+        dat['CHI'] = dat['CHI'][ww]
+        # Make a random selection of 10% of the objects as
+        # the targets -- for now.
         rand = rng.uniform(low=0,high=1,size=dat['RA'].size)
         ww   = np.nonzero( rand<0.10 )[0]
         targ = {}
@@ -99,10 +101,9 @@ if __name__=="__main__":
     with open("mc_lbg_wx.txt","w") as fout:
         fout.write("# Monte-Carlo calculation of wx using {:d} mocks.\n".\
                    format(wxs.shape[0]))
-        fout.write("# Field diameter is {:.2f}deg, area {:.2f}deg2.\n".\
-                   format(diam,area))
-        fout.write("# Sampling by {:.3f} to get {:.1f} LBGs/deg2\n".\
-                   format(fsamp,ntarget))
+        fout.write("# Field "+fname+"\n")
+        fout.write("# Centered on ({:.3f},{:.3f})\n".format(cra,cdc))
+        fout.write("# Number density is {:.3e}\n".format(lbgs.d['nbar']))
         fout.write("# Have {:.1f}+/-{:.2f} LBGs/field.\n".\
                    format(navg,nerr))
         fout.write("# Correlation matrix is:\n")
