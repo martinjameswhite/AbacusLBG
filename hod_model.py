b03472207bc40ccf9d22cc86249d5293d567d7f3
Martin J White <mwhite@berkeley.edu>
2023-03-02 16:04:47 -0800

    Cache newBall.

--- hod_model.py
+++ hod_model.py
@@ -46,6 +46,9 @@ class HODmodel():
             self.newBall  = AbacusHOD(self.sim_params,\
                                       self.HOD_params,\
                                       self.clustering_params)
+        else:
+            for k in ['logM_cut','logM1','sigma','kappa','alpha']:
+                newBall.tracers['LRG'][k] = self.HOD_params['LRG_params'][k]
         want_rsd,write_to_disk = True,False
         self.mock_dict= self.newBall.run_hod(self.newBall.tracers,\
                                              want_rsd,write_to_disk,\
