13aca609429b24a6ef36f6b73e00fc8d975dd5d2
Martin J White <mwhite@berkeley.edu>
2023-03-04 19:02:20 -0800

    For completeness.

---
+++ mc_lbg_wt.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+#
+# Runs a Monte-Carlo loop generating mock LBG catalogs and
+# computing their clustering.
+#
+import numpy as np
+import os
+
+from   fake_lbg  import MockLBG
+from   in_mask   import SurveyMask
+from   rotate_to import rotate_to
+
+from astropy.table     import Table
+from scipy.interpolate import InterpolatedUnivariateSpline as Spline
+
+###import Corrfunc
+from   Corrfunc.mocks.DDtheta_mocks import DDtheta_mocks
+from   Corrfunc.utils import convert_3d_counts_to_cf
+
+
+
+
+def calc_wt(dra,ddc,rra,rdc):
+    """Computes the angular correlation function"""
+    threads= int(os.getenv('OMP_NUM_THREADS',1))
+    bins   = np.geomspace(0.005,0.5,16)
+    DD_cnt = DDtheta_mocks(1,threads,bins,dra,ddc)
+    RR_cnt = DDtheta_mocks(1,threads,bins,rra,rdc)
+    DR_cnt = DDtheta_mocks(0,threads,bins,dra,ddc,RA2=rra,DEC2=rdc)
+    tt,wt  = np.sqrt(bins[:-1]*bins[1:]),\
+             convert_3d_counts_to_cf(dra.size,dra.size,\
+                                     rra.size,rra.size,\
+                                     DD_cnt,DR_cnt,DR_cnt,RR_cnt)
+    return((tt,wt))
+    #
+
+
+
+if __name__=="__main__":
+    # Set up a random number generator with a
+    # fixed seed for reproducability.
+    rng    = np.random.default_rng(1)
+    # Set the name of the field we'll work with and load
+    # the mask, random catalog and radial selection fn.
+    fname= "xmmlss"
+    sfn  = np.loadtxt("selection_fn.txt")
+    mask = SurveyMask('clauds-{:s}-hpmask.fits'.format(fname))
+    tt   = Table.read('clauds-{:s}-rands.fits'.format(fname))
+    # Set the center of the field.
+    cra = np.median(tt['RA'])
+    cdc = np.median(tt['DEC'])
+    print("Setting field center to ({:f},{:f})".format(cra,cdc),flush=True)
+    # Mask the randoms.
+    ww,ran     = mask(tt['RA'],tt['DEC']),{}
+    ran['RA' ] = tt['RA' ][ww]
+    ran['DEC'] = tt['DEC'][ww]
+    # Define the mock catalog, shell and HOD.
+    # 0  12.15  13.55   0.30   1.00   0.75   0.03   6.79e-04
+    lbgs   = MockLBG('hod_big.yaml',None,4383.)
+    params = {'logM_cut':12.15,'logM1':13.55,\
+              'sigma':0.30,'kappa':1.00,'alpha':0.75}
+    lbgs.set_hod(params)
+    lbgs.generate()
+    lbgs.assign_lum(0.25)
+    # Select a field so we have access to the Lside etc.
+    diam   = 4.5 * np.pi/180.
+    lbgs.select(diam,[0.,0.,0.])
+    chi0   = lbgs.d['chi0']
+    Lside  = lbgs.d['Lside']
+    Lx     = Lside/chi0 * 180./np.pi
+    Ly     = Lside/chi0 * 180./np.pi
+    ichi   = 1.0/  chi0 * 180./np.pi
+    # Match the min/max values of chi.
+    chimin,chimax = np.min(lbgs.zpos+chi0),np.max(lbgs.zpos+chi0)
+    print("Raw 3D number density ",lbgs.d['nbar'])
+    sampfn  = Spline(sfn[:,1],1e-3/lbgs.d['nbar']*sfn[:,2],k=1,ext='zeros')
+    sampnrm = np.trapz(sampfn(sfn[:,1]),x=sfn[:,1])
+    # Now do the MC loop.
+    rval,wts,ngals,fchis = None,[],[],[]
+    for i in range(256):
+        # Generate the galaxies.
+        offset = rng.uniform(low=-0.5,high=0.5,size=3)
+        lbgs.select(diam,offset)
+        dat        = {}
+        dat['RA' ] = lbgs.xpos*ichi
+        dat['DEC'] = lbgs.ypos*ichi
+        dat['CHI'] = lbgs.zpos+chi0
+        # Apply radial selection function.
+        rand = rng.uniform(low=0,high=1,size=dat['RA'].size)
+        ww   = np.nonzero( rand<sampfn(dat['CHI']) )[0]
+        dat['RA' ] = dat['RA' ][ww]
+        dat['DEC'] = dat['DEC'][ww]
+        # Rotate the objects to the field center and apply mask.
+        nra,ndc    = rotate_to(dat['RA'],dat['DEC'],cra,cdc)
+        ww         = mask(nra,ndc)
+        dat['RA' ] = nra[ww]
+        dat['DEC'] = ndc[ww]
+        # compute the clustering.
+        bins,wt = calc_wt(dat['RA' ].astype('float'),\
+                          dat['DEC'].astype('float'),\
+                          ran['RA' ].astype('float'),\
+                          ran['DEC'].astype('float'))
+        wts.append(wt)
+        ngals.append(dat['RA'].size)
+    wts  = np.array(wts)
+    wavg = np.mean(wts,axis=0)
+    werr = np.std( wts,axis=0)
+    wcor = np.corrcoef(wts,rowvar=False)
+    navg = np.mean(np.array(ngals,dtype='float'))
+    nerr = np.std( np.array(ngals,dtype='float'))
+    # Now write out some results.
+    with open("mc_lbg_wt.txt","w") as fout:
+        fout.write("# Monte-Carlo calculation of wt using {:d} mocks.\n".\
+                   format(wts.shape[0]))
+        fout.write("# Field "+fname+"\n")
+        fout.write("# Centered on ({:.3f},{:.3f})\n".format(cra,cdc))
+        fout.write("# Number density is {:.3e}\n".format(lbgs.d['nbar']))
+        fout.write("# Have {:.1f}+/-{:.2f} LBGs/field.\n".\
+                   format(navg,nerr))
+        fout.write("# Correlation matrix is:\n")
+        for i in range(bins.size):
+            outstr = "#"
+            for j in range(bins.size): outstr += " {:8.4f}".format(wcor[i,j])
+            fout.write(outstr + "\n")
+        fout.write("# {:>8s} {:>15s} {:>15s}\n".\
+                   format("th[deg]","wt","dwt"))
+        for i in range(bins.size):
+            outstr = "{:10.3f} {:15.5e} {:15.5e}".format(bins[i],wavg[i],werr[i])
+            fout.write(outstr+"\n")
+    #
