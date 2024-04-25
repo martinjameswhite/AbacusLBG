b8bed90ba31bdcb1afc7b689bc601341c7043042
Martin White <martinjameswhite@users.noreply.github.com>
2024-04-25 15:37:14 -0700

    New API for ZCV.

--- prepare_zcv.sh
+++ prepare_zcv.sh
@@ -11,13 +11,15 @@
 module load python
 conda activate abacus
 #
+yaml=./prepare_zcv.yaml
+#
 # This code uses abacusnbody.hod.zcv
 # Run once per simulation.
-#python -m abacusnbody.hod.zcv.ic_fields --path2config ./prepare_zcv.yaml 
+#python -m abacusnbody.hod.zcv.ic_fields --path2config $yaml
 # Run once per redshift and cosmology.
-#python -m abacusnbody.hod.zcv.zenbu_window --path2config ./prepare_zcv.yaml
+#python -m abacusnbody.hod.zcv.zenbu_window --path2config $yaml
+#python -m abacusnbody.hod.zcv.zenbu_window --path2config $yaml --want_xi
 # Run once per simulation and redshift bin.
-#python -m abacusnbody.hod.zcv.advect_fields --path2config ./prepare_zcv.yaml --want_rsd
-# Run once per simulation and redshift bin (needed even if want_rsd is true).
-#python -m abacusnbody.hod.zcv.advect_fields --path2config ./prepare_zcv.yaml
+#python -m abacusnbody.hod.zcv.advect_fields --path2config $yaml --want_rsd
+#python -m abacusnbody.hod.zcv.advect_fields --path2config $yaml --want_rsd --save_3D_power
 #
